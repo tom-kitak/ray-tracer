b1f33fe92c0f6dffb6d643bd1415dbc6adb21da3
Tommy Hu <T.Hu-3@student.tudelft.nl>
2022-10-18 17:02:55 +0200

    implementation for computeShading

--- src/shading.cpp
+++ src/shading.cpp
@@ -5,8 +5,15 @@
 
 const glm::vec3 computeShading(const glm::vec3& lightPosition, const glm::vec3& lightColor, const Features& features, Ray ray, HitInfo hitInfo)
 {
-    // TODO: implement the Phong shading model.
-    return hitInfo.material.kd;
+    glm::vec3 hitPos = ray.origin + ray.t * ray.direction;
+    glm::vec3 viewDir = glm::normalize(ray.origin - hitPos);
+    glm::vec3 lightDir = glm::normalize(lightPosition - hitPos);
+    glm::vec3 reflectDir = glm::reflect(-lightDir, glm::normalize(hitInfo.normal));
+    // glm::vec3 reflectDir = glm::normalize(computeReflectionRay(ray, hitInfo).direction);
+    float specAngle = std::max(glm::dot(reflectDir, viewDir), 0.0f);
+    float specular = std::pow(specAngle, hitInfo.material.shininess);
+    float angle = std::max(glm::dot(glm::normalize(hitInfo.normal), lightDir), 0.0f);
+    return (specular * hitInfo.material.ks * lightColor) + (angle * lightColor * hitInfo.material.kd);
 }
 
 
